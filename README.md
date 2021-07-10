4215f2d02fdf82096bfd665e15d72a5a219abfaa
ahmadjihadalmahaini <ahmadmha1@gmail.com>
2021-07-10 03:11:31 +0300

    lab4

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # About-me
 ## this project is information about me and It includes a set of guessing games
 # Written by ahmad jihad almahahini
+# 
+# 
+
+# Lab02 : we created the project and added 5 questions for the user.
+# Lab03 : we added another 6th and 7th questions.
+# Lab04 : We worked to add function for each question.
--- js/app.js
+++ js/app.js
@@ -1,5 +1,232 @@
+
+
+
 'use strict';
 let score = 0;
+
+
+
+
+function qOne() {
+  let answerOne = prompt('Do you know someone called ahmad?');
+  answerOne = answerOne.toLowerCase();
+  switch (answerOne) {
+    case 'yes':
+    case 'y':
+      score++;
+      alert('you are social');
+      // alert(`you have a score of ${score} /7`);
+      // //console.log(`you have a score of ${score} /7`);
+      break;
+    case 'no':
+    case 'n':
+      alert('you arenot social');
+
+      break;
+
+  }
+}
+
+function qTwo() {
+  let answerTwo = prompt('Do you think I am doing well with software development ?');
+  answerTwo = answerTwo.toLowerCase();
+  switch (answerTwo) {
+    case 'yes':
+    case 'y':
+      alert('Thank you, but I think I need more effort and work');
+      score++;
+      // alert(`you have a score of ${score} /7`);
+      // //console.log(`you have a score of ${score} /7`);
+      break;
+    case 'no':
+    case 'n':
+      alert('Thank you, I will do my best');
+
+
+      break;
+  }
+}
+
+
+
+
+
+
+function qThree() {
+  let football = prompt('Do you think I watch football matches ?');
+  football = football.toLowerCase();
+  switch (football) {
+    case 'yes':
+    case 'y':
+      alert('Yes, I really do :)');
+      score++;
+      // alert(`you have a score of ${score} /7`);
+      //// console.log(`you have a score of ${score} /7`);
+      break;
+    case 'no':
+    case 'n':
+      alert('No, in fact, I am a big fan of football matches');
+
+
+      break;
+
+  }
+}
+
+
+
+
+
+
+function qFour() {
+  let favTeam = prompt('Do you think my favorite team is Real Madrid ?');
+  favTeam = favTeam.toLowerCase();
+  switch (favTeam) {
+    case 'yes':
+    case 'y':
+      alert('It\'s easy to guess Real Madrid is my favorite club');
+      score++;
+      // alert(`you have a score of ${score} /7`);
+      //// console.log(`you have a score of ${score} /7`);
+      break;
+    case 'no':
+    case 'n':
+      alert('Unfortunately, I am a Real Madrid fan :)');
+
+
+
+
+      break;
+
+  }
+}
+
+function qFive() {
+
+  let aboutMe = prompt('Do you think that I am an alien?');
+  aboutMe = aboutMe.toLowerCase();
+  switch (aboutMe) {
+    case 'yes':
+    case 'y':
+      alert('Oh really?');
+
+      alert('Nice try !');
+      break;
+    case 'no':
+    case 'n':
+      alert('Yes!!');
+      score++;
+      // alert(`you have a score of ${score} /7`);
+      //// console.log(`you have a score of ${score} /7`);
+      break;
+
+  }
+}
+
+function qSix() {
+  out: for (let i = 4; i >= 1; --i) {
+
+    let guessNum = prompt('Enter a number from 0 to 10');
+    guessNum = parseInt(guessNum);
+
+
+    // while (guessNum !== 0 && guessNum !== 1 && guessNum !== 2 && guessNum !== 3 && guessNum !== 4 && guessNum !== 5 && guessNum !== 6 && guessNum !== 7 && guessNum !== 8 && guessNum !== 9 && guessNum !== 10) {
+
+
+    guessNum = parseInt(guessNum);
+
+
+
+    if (guessNum == 5) {
+      alert('Well done, you hit the right Number');
+      score++;
+      // alert(`you have a score of ${score} /7`);
+      // //console.log(`you have a score of ${score} /7`);
+      break out;
+    }
+
+    else if (guessNum > 5) {
+      alert('too High');
+      alert('you have a ' + i + ' attempt from   4')
+
+
+
+    }
+    else {
+      alert('too low');
+      alert('you have a ' + i + ' attempt from   4')
+
+
+    }
+
+
+
+  }
+}
+
+
+
+
+
+
+
+function qSeven() {
+  let cars = ['bmw', 'audi', 'ford', 'honda'];
+  ////console.log(cars[0]);
+  lastQus: for (let atmp = 6; atmp >= 1; --atmp) {
+
+
+    let favCar = prompt('Guess my favorite car').toLowerCase();
+    alert('you have a ' + atmp + ' attempt from   6')
+
+    for (let i = 0; i < cars.length; i++) {
+
+      if (favCar === cars[i]) {
+        alert('Great, correct answer !!');
+        score++;
+        // alert(`you have a score of ${score} /7`);
+        //// console.log(`you have a score of ${score} /7`);
+
+
+        break lastQus;
+
+      }
+      if (atmp === 1) {
+        alert('Well, You have exhausted all attempts, My favorite car are ' + cars);
+
+      }
+    }
+
+
+  }
+
+  //// console.log(`you have a score of ${score} /7`);
+
+
+  alert('Thanks for your time, ' + userName + ' You get ' + score + ' / 7');
+  // //console.log('Thanks for your time, ' + userName + ' You get ' + score + ' / 7');
+
+}
+
+
+
+function guessGame() {
+  let game = confirm('Do you want to Play Guessing Game');
+
+  if (game) {
+    alert('well let\'s start ');
+    qOne();
+    qTwo();
+    qThree();
+    qFour();
+    qFive();
+    qSix();
+    qSeven();
+  }
+
+}
+
+
 
 let userName = prompt('What is your name ?');
 
@@ -9,205 +236,5 @@ while (!userName) {
 
 }
 alert('welcome ' + userName);
-console.log('welcome ' + userName)
-
-
-
-let game = confirm('Do you want to Play Guessing Game');
-
-
-
-let answerOne = prompt('Do you know someone called ahmad ?');
-answerOne = answerOne.toLowerCase();
-switch (answerOne) {
-  case 'yes':
-  case 'y':
-    score++;
-    alert('you are social');
-    // alert(`you have a score of ${score} /7`);
-    console.log(`you have a score of ${score} /7`);
-    break;
-  case 'no':
-  case 'n':
-    alert('you arenot social');
-
-    break;
-
-}
-
-
-
-let answerTwo = prompt('Do you think I am doing well with software development ?');
-answerTwo = answerTwo.toLowerCase();
-switch (answerTwo) {
-  case 'yes':
-  case 'y':
-    alert('Thank you, but I think I need more effort and work');
-    score++;
-    // alert(`you have a score of ${score} /7`);
-    console.log(`you have a score of ${score} /7`);
-    break;
-  case 'no':
-  case 'n':
-    alert('Thank you, I will do my best');
-
-
-    break;
-}
-
-
-
-
-
-
-
-
-let football = prompt('Do you think I watch football matches ?');
-football = football.toLowerCase();
-switch (football) {
-  case 'yes':
-  case 'y':
-    alert('Yes, I really do :)');
-    score++;
-    // alert(`you have a score of ${score} /7`);
-    console.log(`you have a score of ${score} /7`);
-    break;
-  case 'no':
-  case 'n':
-    alert('No, in fact, I am a big fan of football matches');
-
-
-    break;
-
-}
-
-
-
-
-
-
-
-
-let favTeam = prompt('Do you think my favorite team is Real Madrid ?');
-favTeam = favTeam.toLowerCase();
-switch (favTeam) {
-  case 'yes':
-  case 'y':
-    alert('It\'s easy to guess Real Madrid is my favorite club');
-    score++;
-    // alert(`you have a score of ${score} /7`);
-    console.log(`you have a score of ${score} /7`);
-    break;
-  case 'no':
-  case 'n':
-    alert('Unfortunately, I am a Real Madrid fan :)');
-
-
-
-
-    break;
-
-}
-
-
-
-let aboutMe = prompt('Do you think that I am an alien?');
-aboutMe = aboutMe.toLowerCase();
-switch (aboutMe) {
-  case 'yes':
-  case 'y':
-    alert('Oh really?');
-
-    alert('Nice try !');
-    break;
-  case 'no':
-  case 'n':
-    alert('Yes!!');
-    score++;
-    // alert(`you have a score of ${score} /7`);
-    console.log(`you have a score of ${score} /7`);
-    break;
-
-}
-
-
-
-out: for (let i = 4; i >= 1; --i) {
-
-  let guessNum = prompt('Enter a number from 0 to 10');
-  guessNum = parseInt(guessNum);
-
-
-  // while (guessNum !== 0 && guessNum !== 1 && guessNum !== 2 && guessNum !== 3 && guessNum !== 4 && guessNum !== 5 && guessNum !== 6 && guessNum !== 7 && guessNum !== 8 && guessNum !== 9 && guessNum !== 10) {
-
-
-  guessNum = parseInt(guessNum);
-
- 
-
-  if (guessNum == 5) {
-    alert('Well done, you hit the right Number');
-    score++;
-    // alert(`you have a score of ${score} /7`);
-    console.log(`you have a score of ${score} /7`);
-    break out;
-  }
-
-  else if (guessNum > 5) {
-    alert('too High');
-    alert('you have a ' + i + ' attempt from   4')
-
-    
-
-  }
-  else {
-    alert('too low');
-  alert('you have a ' + i + ' attempt from   4')
-
-
-  }
-
-
-
-}
-
-
-
-
-
-
-
-
-
-let cars = ['bmw', 'audi', 'ford', 'honda'];
-//console.log(cars[0]);
-lastQus: for (let atmp = 6; atmp >= 1; --atmp) {
-
-
-  let favCar = prompt('Guess my favorite car').toLowerCase();
-  alert('you have a ' + atmp + ' attempt from   6')
-
-  for (let i = 0; i < cars.length; i++) {
-
-    if (favCar === cars[i]) {
-      alert('Great, correct answer !!');
-      score++;
-      // alert(`you have a score of ${score} /7`);
-      console.log(`you have a score of ${score} /7`);
-
-
-      break lastQus;
-
-    }
-    // alert('Well, You have exhausted all attempts, My favorite car are ' + cars);
-
-  }
-
-
-
-}
-// console.log(`you have a score of ${score} /7`);
-
-
-alert('Thanks for your time, ' + userName + ' You get ' + score + ' / 7');
-console.log('Thanks for your time, ' + userName + ' You get ' + score + ' / 7');
+// //console.log('welcome ' + userName)
+guessGame();
